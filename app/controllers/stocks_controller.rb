829dbdfc48ad81fb02bef5ff0ee03007917ad212
giljr <giljr.2009@gmail.com>
2024-09-09 22:57:14 -0400

    🐛Add address rules. Next: user controller.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,6 @@
 class StocksController < ApplicationController
+    # Ensure the user is logged in before accessing these actions
+    before_action :authenticate_user!
   
   def index
     @stocks = Stock.all
